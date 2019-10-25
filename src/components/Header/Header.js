f0d7109ec1da1576c81bbe5f0b80efe5c23f467e
Danilov Dmitriy <dmitry.danilov@coolclever.ru>
2019-10-25 17:46:16 +0300

    material try

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
 class Header extends React.Component{
+
+  useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+
   render() {
     const navItems = [
       {id: 0, title: 'Карта', route: '#/map'},
@@ -8,17 +27,19 @@ class Header extends React.Component{
       {id: 2, title: 'Выйти', route: '#/login'}
     ];
 
+    const classes = this.useStyles;
+
     return (
-      <header>
-        <p>Loft Taxi</p>
-        <ul>
+      <AppBar position="static" style={{ background: '#fff' }}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Loft Taxi
+          </Typography>
           {navItems.map(navItem => (
-            <li key={navItem.id}>
-              <a href={navItem.route} >{navItem.title}</a>                            
-            </li>
-          ))}
-        </ul>
-      </header>
+            <Button  href={navItem.route}>{navItem.title}</Button>
+          ))} 
+        </Toolbar>
+      </AppBar>
     )
   }
 }
