f8b7427bf34761c1377ee9f7fc49eeb8abb5b8c2
Dmitry <dima-danilov1995@mail.ru>
2019-11-26 21:10:08 +0300

    route line

--- src/App/containers/Dashboard/Map/MapBox.jsx
+++ src/App/containers/Dashboard/Map/MapBox.jsx
@@ -1,28 +1,93 @@
 import React, { Component } from 'react';
 import mapboxgl from "mapbox-gl";
+import { connect } from 'react-redux';
+import { routeSelector } from "./../../../store/selectors";
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGR2diIsImEiOiJjazJjeGp1djYwYTk4M29xYjZzNTdqcDJvIn0.3AlKVyy5UkxkH_jY_QLTIQ';
+
+const mapStateToProps = state => {
+  return {
+    routCoordinates: routeSelector(state),
+  };
+};
 
 class MapBox extends Component {
   constructor(props) {
     super(props);
     this.mapContainer = React.createRef();
     this.state = {
-      lat: 56.326797, 
-      lng: 44.006516,
-      zoom: 11,
+      lat: 59.939095, 
+      lng: 30.315868,
+      zoom: 12,
     };
   }
 
+  map = null;
+
   componentDidMount() {
     const { lng, lat, zoom } = this.state;
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/ddvv/ck2dcj6y418wr1fo4tomxiips',
       center: [lng, lat],
-      zoom
+      zoom: zoom
     });
   }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      const { routCoordinates } = this.props;
+
+      if (this.map.getLayer("route")) {
+        this.map.removeLayer("route");
+        this.map.removeSource("route");
+      }
+
+      if (routCoordinates && routCoordinates.length > 0) {
+        this.renderRoute();
+      }
+    }
+  }
+
+
+  renderRoute = () => {
+    const { routCoordinates } = this.props;
+
+    console.log(routCoordinates);
+
+    // this.map.on('load', () => {
+    this.map.addLayer({
+      id: "route",
+      type: "line",
+      source: {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {
+            color: "#ffc617",
+          },
+          geometry: {
+            type: "LineString",
+            coordinates: routCoordinates,
+          },
+        },
+      },
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-width": 6,
+        "line-color": "#ffc617",
+      },
+    });
+    // })    
+
+    this.map.flyTo({
+      center: routCoordinates[0],
+      zoom: 12,
+    });
+  };
 
   render() { 
     const style = {
@@ -33,4 +98,7 @@ class MapBox extends Component {
   }
 }
  
-export default MapBox;
+export default connect(
+  mapStateToProps,
+  null
+)(MapBox);
